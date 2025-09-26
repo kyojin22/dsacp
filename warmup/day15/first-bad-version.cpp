64f3ad3651082b09d867dc804876897a7e7ab287
Muhammadbobur <bobur0218programmer@gmail.com>
2025-09-26 22:01:45 +0500

    solve: leetcode #278 and #33

---
+++ warmup/day15/first-bad-version.cpp
@@ -0,0 +1,26 @@
+// 26 September, 2025
+//
+// am i fired?
+//
+
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 0;
+        int r = n;
+        while (l < r) {
+            int m = ((r - l) / 2) + l;
+            if (isBadVersion(m)) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return r;
+    }
+};
+
